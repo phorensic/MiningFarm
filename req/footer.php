14615cb1050a6a4cdabc4a087a47ddf3740c8b81
Matt <phorensic@.(none)>
2011-06-24 20:49:00 -0700

    Support Link in Footer

--- req/footer.php
+++ req/footer.php
@@ -7,7 +7,7 @@
 
 		<span align="center">
 		<a href="http://carbonism.deviantart.com/" target="_BLANK"><?php echo gettext("Wallpaper By");?> Carbonism</a><br/>
-		<b><?php echo gettext("Donations");?><br/>1Fc2ScswXAHPUgj3qzmbRmwWJSLL2yv8Q</b>
+		<b><?php echo gettext("Donations");?><br/>1Fc2ScswXAHPUgj3qzmbRmwWJSLL2yv8Q</b> | <a href="mailto:support@yourdomain.com?subject=Mining Farm Support">Contact Support</a>
 		</span>
 </div><br/>
 >>>>>>> f9332a8ad0cd4e27505f162718c69fc8ea297aa7
