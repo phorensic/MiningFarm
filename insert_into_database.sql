71796ec65bcb153e07a71c3544bad0778a2efa47
Xen Land <xennetwork@hotmail.com>
2011-06-26 17:19:15 -0700

    Topright display error fix

--- insert_into_database.sql
+++ insert_into_database.sql
@@ -3,11 +3,17 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Jun 26, 2011 at 02:43 AM
+-- Generation Time: Jun 26, 2011 at 05:03 PM
 -- Server version: 5.1.54
 -- PHP Version: 5.3.5-1ubuntu7.2
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `miningfarm`
@@ -33,8 +39,6 @@ CREATE TABLE IF NOT EXISTS `accountBalance` (
 -- Dumping data for table `accountBalance`
 --
 
-INSERT INTO `accountBalance` (`id`, `userId`, `balance`, `payoutAddress`, `threshhold`) VALUES
-(40, 43, '0.00', '', '');
 
 -- --------------------------------------------------------
 
@@ -48,14 +52,12 @@ CREATE TABLE IF NOT EXISTS `blogPosts` (
   `title` varchar(255) NOT NULL,
   `message` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
 
 --
 -- Dumping data for table `blogPosts`
 --
 
-INSERT INTO `blogPosts` (`id`, `timestamp`, `title`, `message`) VALUES
-(5, 1307966616, 'We Mine Coins!', 'Welcome to the <b>Mining farm</b>, if you''ve stumbled across here by accident, You are in for a treat. You can use your computer to help the community mine an Internet commodity known as Bitcoins. You can buy all sorts of stuff with bitcoins like Amazon giftcards, web servers, MMORPG game, or even trade it for cash!\r\n<br/>\r\n<br/>\r\n<h3>Okay, So how do I get BitCoins?</h3>\r\nBitcoins are obtained by a term known as <i>Mining</i>, which is just an easier way to say <i><b>Hashing transactions across the network with Cryptographic algorithims</i></b>. Mining involves your computer to run a program to encrypt transactions with either an on-board processor or your Video Card(Recommended). When there are enough transactions encrypted you obtain a virtual object called a <i>Block</i> which will then be sent out to the network for verification. After a Mining Pool has found a <i>block</i> they will split up the reward according to how many transactions your CPU or GPU(Video Card) executed.\r\n<br/><br/>\r\n<h3>And I use them, how?</h3>\r\nYou can use Bitcoins by downloading a Bitcoin wallet for free over at <a href="http://bitcoin.org">www.BitCoin.org</a>. After you''ve obtained a free Bitcoin wallet you can login to your account here and type in one of your many assigned <i>wallet address</i> to have the payment sent to. Upon payment you are free do to what you want with your Bitcoins. Here is a rough list of websites that accept Bitcoins as payment. <a href="https://en.bitcoin.it/wiki/Trade" target="_BLANK">Sites that accept bitcoins</a>');
 
 -- --------------------------------------------------------
 
@@ -70,14 +72,12 @@ CREATE TABLE IF NOT EXISTS `networkBlocks` (
   `txid` varchar(255) NOT NULL,
   `confirms` int(255) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=12 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `networkBlocks`
 --
 
-INSERT INTO `networkBlocks` (`id`, `blockNumber`, `timestamp`, `txid`, `confirms`) VALUES
-(11, 26700, 1309081364, '', 0);
 
 -- --------------------------------------------------------
 
@@ -92,12 +92,11 @@ CREATE TABLE IF NOT EXISTS `pool_worker` (
   `password` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
   UNIQUE KEY `username` (`username`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=22 ;
 
 --
 -- Dumping data for table `pool_worker`
 --
-
 
 -- --------------------------------------------------------
 
@@ -116,14 +115,12 @@ CREATE TABLE IF NOT EXISTS `shares` (
   `reason` varchar(50) DEFAULT NULL,
   `solution` varchar(257) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=703 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3240 ;
 
 --
 -- Dumping data for table `shares`
 --
-
-
--- --------------------------------------------------------
+ --------------------------------------------------------
 
 --
 -- Table structure for table `shares_history`
@@ -141,12 +138,11 @@ CREATE TABLE IF NOT EXISTS `shares_history` (
   `reason` varchar(50) DEFAULT NULL,
   `solution` varchar(257) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=19979 ;
 
 --
 -- Dumping data for table `shares_history`
 --
-
 
 -- --------------------------------------------------------
 
@@ -160,7 +156,7 @@ CREATE TABLE IF NOT EXISTS `stats_poolMHashHistory` (
   `averageMhash` varchar(20) NOT NULL,
   `timestamp` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1937 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1938 ;
 
 --
 -- Dumping data for table `stats_poolMHashHistory`
@@ -179,7 +175,7 @@ CREATE TABLE IF NOT EXISTS `stats_userMHashHistory` (
   `mhashes` varchar(20) NOT NULL,
   `timestamp` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1937 ;
+) ENGINE=MyISAM  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1938 ;
 
 --
 -- Dumping data for table `stats_userMHashHistory`
@@ -200,15 +196,16 @@ CREATE TABLE IF NOT EXISTS `websiteSettings` (
   `browserTitle` varchar(255) NOT NULL,
   `cashoutMinimum` varchar(5) NOT NULL COMMENT 'The minimum balance required before a user can cash out',
   `serverFeePercentage` varchar(20) NOT NULL COMMENT 'Server fee in percents',
-  `tradeHillWorth` varchar(20) NOT NULL COMMENT 'Current worth of bitcoins'
+  `tradeHillWorth` varchar(20) NOT NULL COMMENT 'Current worth of bitcoins',
+  `currencyData` varchar(20) NOT NULL COMMENT 'Three letter identifiers what the general default currency should be set too'
 ) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `websiteSettings`
 --
 
-INSERT INTO `websiteSettings` (`header`, `noreplyEmail`, `confirmEmailPrefix`, `slogan`, `browserTitle`, `cashoutMinimum`, `serverFeePercentage`, `tradeHillWorth`) VALUES
-('Mining Pool v3', 'no-reply@yourdomain.com', 'Welcome you "Your pool name here" glad you are interested in our services, In order to activate your account you must click the link provided and you will be allowed immediate login access past this point. Thank you for you time.', 'IP:66.197.184.28 · PORT: 8341', 'Mining Pool', '1', '', '15.30123402');
+INSERT INTO `websiteSettings` (`header`, `noreplyEmail`, `confirmEmailPrefix`, `slogan`, `browserTitle`, `cashoutMinimum`, `serverFeePercentage`, `tradeHillWorth`, `currencyData`) VALUES
+('Mining Pool v4', 'no-reply@yourdomain.com', 'Welcome you "Your pool name here" glad you are interested in our services, In order to activate your account you must click the link provided and you will be allowed immediate login access past this point. Thank you for you time.', 'IP:66.197.184.28 · PORT: 8341', 'Mining Pool', '1', '3', '15.3', 'tradehill-USD');
 
 -- --------------------------------------------------------
 
@@ -236,3 +233,4 @@ CREATE TABLE IF NOT EXISTS `websiteUsers` (
 --
 -- Dumping data for table `websiteUsers`
 --
+
